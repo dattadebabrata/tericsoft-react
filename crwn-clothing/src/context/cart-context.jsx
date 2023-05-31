554c1c515288fd3fabe4413aa18544168cf92fb3
im-Debabrata <debabrata@tericsoft.com>
2023-05-31 13:12:07 +0530

    added reducers to context api

--- crwn-clothing/src/context/cart-context.jsx
+++ crwn-clothing/src/context/cart-context.jsx
@@ -1,4 +1,5 @@
-import {createContext, react, useState, useEffect} from "react";
+import {createContext, react, useReducer, useEffect} from "react";
+import {createAction} from "../utils/reducer/reducer.utils";
 
 const addCartItem = (cartItems, productToAdd) => {
     //find cartItems contains productToAdd;
@@ -33,43 +34,88 @@ const clearCartItem = (cartItems, cartItemToClear) => cartItems.filter(cartItem 
 
 
 export const CartContext = createContext({
-    isCartOpen: false,
     setIsCartOpen: () => {
     },
-    cartItems: [],
     addItemToCart: () => {
     },
     removeItemFromCart: () => {
     },
     clearItemFromCart: () => {
     },
+});
+
+const INITIAL_STATE = {
     cartCount: 0,
-    cartTotal: 0
-})
+    cartTotal: 0,
+    cartItems: [],
+    isCartOpen: false,
+}
+
+const CART_ACTION_TYPES = {
+    SET_CART_ITEMS: 'SET_CART_ITEMS',
+    SET_IS_CART_OPEN: 'SET_IS_CART_OPEN',
+}
+
+const cartReducer = (state, action) => {
+    const {type, payload} = action;
+    switch (type) {
+        case CART_ACTION_TYPES.SET_CART_ITEMS:
+            return {
+                ...state,
+                ...payload,
+            }
+        case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+            return {
+                ...state,
+                isCartOpen: payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in cartReducer`);
+    }
+}
 export const CartProvider = ({children}) => {
-    const [isCartOpen, setIsCartOpen] = useState(false);
-    const [cartItems, setCartItems] = useState([]);
-    const [cartCount, setCartCount] = useState(0);
-    const [cartTotal, setCartTotal] = useState(0);
+    const [{cartItems, cartCount, cartTotal, isCartOpen}, dispatch] = useReducer(cartReducer, INITIAL_STATE);
 
-    useEffect(() => {
-        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
-        setCartCount(newCartCount);
-    }, [cartItems])
+    const updateCartItemsReducer = (newCartItems) => {
+        const newCartCount = newCartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+        const newCartTotal = newCartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0);
 
-    useEffect(() => {
-        const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0);
-        setCartTotal(newCartTotal);
-    }, [cartItems])
+        dispatch(
+            createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
+                cartItems: newCartItems,
+                cartCount: newCartCount,
+                cartTotal: newCartTotal
+            }))
+        ;
+        /*
+        generate newCartTotal
+        generate newCartItem
+        generate newCartCount
+
+
+        dispatch action with payload = {
+        newCartItems,
+        newCartTotal,
+        newCartCount
+        }
+
+        */
+    }
 
     const addItemToCart = (productToAdd) => {
-        setCartItems(addCartItem(cartItems, productToAdd));
+        const newCartItems = addCartItem(cartItems, productToAdd);
+        updateCartItemsReducer(newCartItems);
     }
     const removeItemToCart = (cartItemToRemove) => {
-        setCartItems(removeCartItem(cartItems, cartItemToRemove));
+        const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+        updateCartItemsReducer(newCartItems);
     }
     const clearItemFromCart = (cartItemToClear) => {
-        setCartItems(clearCartItem(cartItems, cartItemToClear));
+        const newCartItems = clearCartItem(cartItems, cartItemToClear);
+        updateCartItemsReducer(newCartItems);
+    }
+    const setIsCartOpen = (bool) => {
+        dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool));
     }
     const value = {
         isCartOpen,
--- crwn-clothing/src/context/user.contexts.jsx
+++ crwn-clothing/src/context/user.contexts.jsx
@@ -1,25 +1,52 @@
-import {createContext,useState,useEffect} from "react";
-
-import {onAuthStateChangedListener,createUserDocumentFromAuth} from "../utils/firebase/firebase.utils";
+import {createContext, useEffect, useReducer} from "react";
+import {onAuthStateChangedListener, createUserDocumentFromAuth} from "../utils/firebase/firebase.utils";
+import {createAction} from "../utils/reducer/reducer.utils";
 
 export const UserContext = createContext({
-    currentUser:null,
-    setCurrentUser:()=>null,
-})
+    currentUser: null,
+    setCurrentUser: () => null,
+});
+
+export const UserActionTypes = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+    console.log("state", state);
+    console.log("Dispatched")
+    console.log("action", action);
+    const {type, payload} = action;
+
+    switch (type) {
+        case UserActionTypes.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload,
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null,
+}
 
 export const UserProvider = ({children}) => {
-    const [currentUser,setCurrentUser]=useState(null);
-    const value = {currentUser,setCurrentUser};
+    const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE);
+    console.log('currentUser', currentUser);
+    const setCurrentUser = (user) => dispatch(createAction(UserActionTypes.SET_CURRENT_USER, user));
+    const value = {currentUser, setCurrentUser};
     useEffect(() => {
         const unsubscribe = onAuthStateChangedListener((user) => {
             console.log(user)
             if (user) {
-                 createUserDocumentFromAuth(user);
+                createUserDocumentFromAuth(user);
             }
             setCurrentUser(user)
         })
     }, []);
-    
+
     return (
         <UserContext.Provider value={value}>
             {children}
